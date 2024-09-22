004c974d101d632afe1feec67aed12a7002c2795
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-22 19:37:46 +0100

    explaining the reason main and the goroutine are in sync

--- concurrency-2/main.go
+++ concurrency-2/main.go
@@ -14,5 +14,7 @@ func main() {
 
 	msg := <-myChannel // this line of code here is blocking since it is receiving from a channel
 	// the main channel either waits for this to be closed or for the message to be received from the channel
+
+	// line 15 and 16 is simply telling us that the main and the goroutine are in sync.
 	fmt.Println(msg)
 }
