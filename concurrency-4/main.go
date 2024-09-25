d7fd27c602667cc102d8aa86abafe96ccbc82be3
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-09-25 12:16:39 +0100

    create  variables synchronization

---
+++ concurrency-4/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"sync"
+)
+
+func main() {
+	// use a synchronisation primitive; declare variable of type mutex
+	var m sync.Mutex
+
+	ch := make(chan int) // create a channel
+
+}
